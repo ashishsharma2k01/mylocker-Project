bb7108f2918b1d23b7da5fe5fb9882578888739e
Ashish Sharma <ashishsharma2k01@gmail.com>
2026-06-10 21:37:06 +0530

    prop issue

--- Frontend/src/Components/UploadBox.jsx
+++ Frontend/src/Components/UploadBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Uploadbox = () => {
+const Uploadbox = ({setFiles}) => {
 
     const [loading, setloading] = useState(false);
     const [file, setfile] = useState(null);
